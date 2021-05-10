5e66802a3a254eb245153e38f832cb0f61d0dd05
Your Name <hr5359@srmist.edu.in>
2021-05-10 18:50:07 +0530

    Disease prediction bugs fixed

--- src/components/DiseasePred/DiseaseDetection.js
+++ src/components/DiseasePred/DiseaseDetection.js
@@ -16,7 +16,7 @@ function DiseaseDetection() {
                 <h2>Breast Cancer</h2>
                 <p>With more that 1 million cases in India every year, leading to dangerous complication if left
                     unchecked.</p>
-                <a href="https://breastcancerwomenessentials.herokuapp.com/" target="blank">Predict Now</a>
+                <a href="https://breastcancerdetectionweb.herokuapp.com/" target="blank">Predict Now</a>
             </div>
         </div>
 
@@ -28,7 +28,7 @@ function DiseaseDetection() {
             <div class="content">
                 <h2>Diabetes Checkup </h2>
                 <p>About 212 million women worldwide have diabetes. So, take a test now for eary treatment.</p>
-                <a href="https://diabetespredwomenessential.herokuapp.com/" target="blank">Predict Now</a>
+                <a href="https://diabetiespredictionweb.herokuapp.com/" target="blank">Predict Now</a>
             </div>
         </div>
 
@@ -39,7 +39,7 @@ function DiseaseDetection() {
             <div class="content">
                 <h2>Heart Diseases</h2>
                 <p>Heart disease can cause many potentially fatal complications if left untreated.</p>
-                <a href="https://heartdetectionwomenessential.herokuapp.com" target="blank">Predict Now</a>
+                <a href="https://heartdiseasepredictionweb.herokuapp.com/" target="blank">Predict Now</a>
             </div>
         </div>
 
@@ -50,7 +50,7 @@ function DiseaseDetection() {
             <div class="content">
                 <h2>Kidney Diseases</h2>
                 <p>Chronic kidney disease can affect almost every part of your body.</p>
-                <a href="https://kidneydiseasewomenessential.herokuapp.com/" target="blank">Predict Now</a>
+                <a href="https://kidneydiseasepredictionweb.herokuapp.com/" target="blank">Predict Now</a>
             </div>
         </div>
     </div>
